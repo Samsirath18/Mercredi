5be0815d940f6cb84817d1fc9057b41beb273488
Samadou02 <samadouaboudou925@gmail.com>
2025-04-03 14:21:12 +0100

    modifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './component/Partials/NavBarStyle.css'
+
+import NavBar from './component/Partials/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NavBar/>
   </StrictMode>,
 )
